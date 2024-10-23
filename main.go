d472d04a6bcfe97af97478702677392c76f2c15c
Sandpaul <145486117+Sandpaul@users.noreply.github.com>
2024-10-23 11:13:01 +0100

    fix FetchData, and new lines to print statements, implement Main().

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Create a key-value store:
 //// Store(string key, string value)
@@ -25,43 +28,48 @@ type operation struct {
 
 var requests chan operation = make(chan operation)
 
-func CreateStoreRequest(key string, value string) {
-	storeRequest := operation{action: "store", key: key, value: value}
-
-	requests <- storeRequest
-}
-
-func CreateFetchRequest(key string) string {
-	fetchedData := make(chan string)
-
-	op := operation{action: "fetch", key: key, value: ""}
+func StoreData(key string, value string) {
+	op := operation{action: "store", key: key, value: value}
 
 	requests <- op
-	return <- fetchedData
+}
+
+func FetchData(key string) string {
+	fetchedData := make(chan string)
+
+	op := operation{action: "fetch", key: key, value: "", response: fetchedData}
+
+	requests <- op
+	return <-fetchedData
+}
+
+func monitorRequests() {
+	dataStore := make(map[string]string)
+	
+	for op := range requests {
+		switch op.action {
+		case "store":
+			fmt.Printf("Saving %s: %s to data store...\n", op.key, op.value)
+			dataStore[op.key] = op.value
+			fmt.Println("Data saved successfully.")
+		case "fetch":
+			fmt.Printf("Fetching %s from data store...\n", op.key)
+			op.response <- dataStore[op.key]
+		}
+	}
 }
 
 func Start() {
 	go monitorRequests()
 }
 
-func monitorRequests() {
-	dataStore := make(map[string]string)
-
-	for op := range requests {
-		switch op.action {
-		case "store":
-			fmt.Printf("Saving %s: %s to data store...", op.key, op.value)
-			dataStore[op.key] = op.value
-			fmt.Println("Data saved successfully.")
-		case "fetch":
-			fmt.Printf("Fetching %s from data store...", op.key)
-			op.response <- dataStore[op.key]
-		}
-	}
-}
-
-
-
 func main() {
-	fmt.Println("Hello, World!")
+	Start()
+
+	go StoreData("name", "paul")
+	go StoreData("age", "34")
+	go fmt.Println("name:" + FetchData("name"))
+	go fmt.Println("age:" + FetchData("age"))
+
+	time.Sleep(15 * time.Second)
 }
