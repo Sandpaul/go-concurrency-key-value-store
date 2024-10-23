7e94fe74c0b8aac9063ad06d2aca862e7bccf604
Sandpaul <145486117+Sandpaul@users.noreply.github.com>
2024-10-23 10:06:34 +0100

    create operation struct and requests chan.

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+// Create a key-value store:
+//// Store(string key, string value)
+//// Fetch(string key) -> returns the value
+//// Start()
+//// Stop()
+
+// Apply the Actor model:
+//// As per the slides
+//// Protected resource: a map holding the data
+
+// Constraints:
+//// Cannot use sync.Map
+//// Cannot use sync.Mutex
+
+type operation struct {
+	action string
+	parameter string
+	response chan string
+}
+
+var requests chan operation = make(chan operation)
+
+
 func main() {
 	fmt.Println("Hello, World!")
 }
